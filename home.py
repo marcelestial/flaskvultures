f2322da9b6a3e6eba348cf9da8b6d9ec76a5d5e1
Marcel Hood <marcel@nottingham.spaceside.com>
2019-11-03 15:50:59 -0800

    starting rest of CRUD functions

--- home.py
+++ home.py
@@ -1,15 +1,27 @@
 from models import *
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-	vultures = Vulture.select().order_by(Vulture.speciesId.asc())
-	return render_template('home.html', vultures=vultures)
+  vultures = Vulture.select().order_by(Vulture.speciesId.asc())
+  return render_template('home.html', vultures=vultures)
 
 @app.route("/detail")
 def detail():
-	return render_template('detail.html')
+  speciesId = request.args.get('id') #snag 'id' argument from url
+  speciesName = Vulture.get(Vulture.speciesId == speciesId).speciesName
+  description = Vulture.get(Vulture.speciesId == speciesId).description
+  return render_template('detail.html', speciesId=speciesId, speciesName=speciesName, description=description)
+
+@app.route("/add")
+def add():
+  return render_template('add.html')
+
+@app.route("/edit")
+def edit():
+  speciesId = request.args.get('id') #snag 'id' argument from url
+  return render_template('edit.html', speciesId=speciesId)
 
 if __name__ == "__main__":
 	app.run(debug=True)
